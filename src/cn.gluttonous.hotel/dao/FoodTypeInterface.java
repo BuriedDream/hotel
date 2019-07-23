6618be4a7aa8b0b787bed1758bb1ae9a3ebee3b8
BuriedDream <xfmdenw@163.com>
2019-07-23 17:46:27 +0800

    Initial commit

---
+++ src/cn.gluttonous.hotel/dao/FoodTypeInterface.java
@@ -0,0 +1,11 @@
+package cn.gluttonous.hotel.dao;
+
+/**
+ * @title: hotel
+ * @Description:
+ * @Author: liam
+ * @Date: 2019/7/23
+ * @Version: 1.0
+ **/
+public interface FoodTypeInterface {
+}
---
+++ src/cn.gluttonous.hotel/service/FoodTypeServiceInterface.java
@@ -0,0 +1,12 @@
+package cn.gluttonous.hotel.service;
+
+/**
+ * @title: hotel
+ * @ClassName FoodTypeServiceInterface.java
+ * @Description:
+ * @Author: liam
+ * @Date: 2019/7/23
+ * @Version: 1.0
+ **/
+public interface FoodTypeServiceInterface {
+}
